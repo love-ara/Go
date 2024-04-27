ef918ec9aaa57e3b926b4b3107420adff83bc505
love-ara <aramideotenaike@gmail.com>
2024-04-27 13:46:11 +0100

    commit

---
+++ chapter2/strings.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello" + "World")
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	fmt.Println(321325 * 424521)
+}
---
+++ dietelDietel/chapter4_4.19/salesCommissionCalculator.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func salesCommissionCalculator() {
+	fmt.Println("Sales commission calculator")
+
+}
